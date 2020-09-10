25e926b87b9c811c333344b4f2ab316feeeb2b6a
Danielle Lynch <danielle.lynch@shopify.com>
2020-09-10 10:55:57 -0400

    edit refactor

--- to-do-app/to-do-2.js
+++ to-do-app/to-do-2.js
@@ -1,40 +1,8 @@
-let toDo = [];
+let toDo = getSavedToDos();
 
 const filters = {
   searchTerms: "",
   hideCompleted: false,
-};
-
-const toDoJSON = localStorage.getItem("todo");
-
-if (toDoJSON !== null) {
-  toDo = JSON.parse(toDoJSON);
-}
-
-const renderToDos = function (toDo, filters) {
-  let filteredNotes = toDo.filter(function (todo) {
-    const searchTextMatch = todo.title
-      .toLowerCase()
-      .includes(filters.searchTerms.toLowerCase());
-    const completedTextMatch = !filters.hideCompleted || !todo.completed;
-    return searchTextMatch && completedTextMatch;
-  });
-
-  const incompleteToDos = toDo.filter(function (todo) {
-    return !todo.completed;
-  });
-
-  document.querySelector("#todos").innerHTML = "";
-
-  const summary = document.createElement("h2");
-  summary.textContent = `You have ${incompleteToDos.length} left to do `;
-  document.querySelector("#todos").appendChild(summary);
-
-  filteredNotes.forEach(function (todo) {
-    const noteEl = document.createElement("p");
-    noteEl.innerHTML = todo.title;
-    document.querySelector("#todos").appendChild(noteEl);
-  });
 };
 
 renderToDos(toDo, filters);
@@ -44,24 +12,6 @@ document.querySelector("#search-text").addEventListener("input", function (e) {
   console.log(filters.searchTerms);
   renderToDos(toDo, filters);
 });
-
-// document.querySelector("#for-fun").addEventListener("change", function (e) {
-//   if (e.target.checked) {
-//     const completed = toDo.filter(function (todos) {
-//       return !todos.completed;
-//     });
-//     renderToDos(completed, filters);
-//   } else {
-//     renderToDos(toDo, filters);
-//   }
-// });
-
-//alternative to the above checkbox logic
-
-// document.querySelector("#for-fun").addEventListener("change", function (e) {
-//   filters.hideCompleted = e.target.checked;
-//   renderToDos(toDo, filters);
-// });
 
 document.querySelector("#new-todo").addEventListener("submit", function (e) {
   e.preventDefault;
